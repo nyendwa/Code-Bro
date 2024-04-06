3dd7ee98ccc73b49237d03456b6a0834ce394e93
nyendwa <nyendwa@yahoo.com>
2024-04-06 02:01:25 +0200

    I am working with DOM manipulation in Js and html

---
+++ index.js
@@ -0,0 +1,22 @@
+// let username;
+// username = window.prompt("Enter your your username: ");
+// console.log(username);
+
+
+// let username;
+// document.getElementById('mysubmit').onclick = function () {
+//     username = document.getElementById('myText').value;
+//     document.getElementById('myH1').textContent = `Hello ${username}`;
+// }
+
+const PI = 3.14159;
+let radius;
+let circumference;
+
+document.getElementById('mySubmit').onclick = function () {
+    radius = document.getElementById('myText').value;
+    radius=Number(radius);
+    circumference = 2 * PI * radius;
+    document.getElementById('myH1').textContent ='The Circumference is: '+circumference + ' cm';
+    console.log(circumference);
+}
